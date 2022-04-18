0f7b66561b82eecfead093199adb6010dd95d16e
Ungvári Krisztián <krisz270@gmail.com>
2022-04-18 12:40:22 +0200

    Add project files.

---
+++ BalKezesek/Jatekos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalKezesek
+{
+    class Jatekos
+    {
+        string nev;
+        DateTime elso;
+        DateTime utolso;
+        byte suly;
+        byte magassag;
+
+        public string Nev { get => nev; set => nev = value; }
+        public DateTime Elso { get => elso; set => elso = value; }
+        public DateTime Utolso { get => utolso; set => utolso = value; }
+        public byte Suly { get => suly; set => suly = value; }
+        public byte Magassag { get => magassag; set => magassag = value; }
+
+        public double MagassagCm {get => magassag* 2.54;}
+
+        public Jatekos (string sor)
+        {
+            string[] daraboltSor = sor.Split(';');
+            Nev = daraboltSor[0];
+            Elso = Convert.ToDateTime(daraboltSor[1]);
+            Utolso = Convert.ToDateTime(daraboltSor[2]);
+            Suly = Convert.ToByte(daraboltSor[3]);
+            Magassag = Convert.ToByte(daraboltSor[4]);
+        }
+    }
+}
---
+++ BalKezesek/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalKezesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Jatekos> jatekosok= new List<Jatekos>();
+            foreach (string jatekos in File.ReadAllLines("balkezesek.csv").Skip(1))
+            {
+                jatekosok.Add(new Jatekos(jatekos));
+            }
+
+            Console.WriteLine("3. feladat: {0}", jatekosok.Count);
+
+            Console.WriteLine("4. feladat: ");
+            foreach (Jatekos jatekos in jatekosok)
+            {
+                if (jatekos.Utolso.Year == 1999 && jatekos.Utolso.Month == 10)
+                {
+                    Console.WriteLine("\t{0},{1:F1}cm",jatekos.Nev , jatekos.MagassagCm);
+                }
+            }
+            Console.WriteLine("5. feladat: ");
+            int ev;
+            do
+            {
+                Console.Write("Kérek egy 1990 és 1999 közötti évszámot!: ");
+                ev = Convert.ToInt32(Console.ReadLine());
+                if (ev < 1990 || ev > 1999)
+                {
+                    Console.Write("Hibás adat!");
+                }
+            } while (ev < 1990 || ev > 1999);
+
+            // 6 feladat innen kezdődik
+
+            int jatekosokSzama = 0;
+            int sulyokOsszege = 0;
+            foreach (Jatekos jatekos in jatekosok)
+            {
+                if (jatekos.Elso.Year <= 0 || jatekos.Utolso.Year >=0)
+                {
+                    jatekosokSzama++;
+                    sulyokOsszege += jatekos.Suly;
+                }
+            }
+            double atlag = (double)sulyokOsszege / jatekosokSzama;
+            Console.WriteLine("6. feladat: {0:F2}font", atlag);
+
+
+            Console.WriteLine("A program vége!");
+            Console.ReadKey();
+        }
+    }
+}
